fda01e189099649cc5de1fa2200df79ca8ebfa31
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-09-26 13:27:19 -0400

    typescript

--- main.ts
+++ main.ts
@@ -6,16 +6,20 @@
 */
 
 // Variables
+let temperature : number
 let temperatureKelvin : number
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// input
 input.onButtonPressed(Button.A, function() {
+  //input
+  temperature = input.temperature()
+
+  //Process
   temperatureKelvin = (temperatureKelvin + 273.15)
   temperatureKelvin = Math.round(temperatureKelvin)
+
+ // output
+  basic.showString('The temperature is ' + temperatureKelvin.toString() + 'K,')
 })
-// output answer
-basic.clearScreen()
-basic.showString('The temperature is ' + temperatureKelvin.toString() + 'K,')
