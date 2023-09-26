c9aae94e500d14919409b9d6468aa8b1ccc324d7
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-09-26 13:07:02 -0400

    Typescript Version

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Giordan Zeina
+ * Created on: Sep 2023
+ * This program shows the temperature of the microbit in Kelvin
 */
 
-basic.showString('Hello, World!')
+// Variables
+let temperature : number
+let temperatureKelvin : number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+// input
+input.onButtonPressed(Button.A, function() { 
+  temperature = input.temperature()
+  temperatureKelvin = (temperatureKelvin + 273.15)
+  temperatureKelvin = Math.round(temperatureKelvin)
+})
+// output answer
+basic.clearScreen()
+basic.showString("The temperature is " + temperatureKelvin.toString() + "K,")
